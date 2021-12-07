aa0696509ed7745e86669f11f651599476363a64
shellingford330 <pg.econ4869@gmail.com>
2021-12-07 23:01:42 +0900

    fix: use object-fit (without calculating image size)

--- client/src/components/foundation/CoveredImage/CoveredImage.jsx
+++ client/src/components/foundation/CoveredImage/CoveredImage.jsx
@@ -1,5 +1,3 @@
-import classNames from 'classnames';
-import sizeOf from 'image-size';
 import React from 'react';
 
 import { useFetch } from '../../../hooks/use_fetch';
@@ -18,41 +16,20 @@ import { fetchBinary } from '../../../utils/fetchers';
 const CoveredImage = ({ alt, src }) => {
   const { data, isLoading } = useFetch(src, fetchBinary);
 
-  const imageSize = React.useMemo(() => {
-    return data !== null ? sizeOf(Buffer.from(data)) : null;
-  }, [data]);
-
   const blobUrl = React.useMemo(() => {
     return data !== null ? URL.createObjectURL(new Blob([data])) : null;
   }, [data]);
-
-  const [containerSize, setContainerSize] = React.useState({ height: 0, width: 0 });
-  /** @type {React.RefCallback<HTMLDivElement>} */
-  const callbackRef = React.useCallback((el) => {
-    setContainerSize({
-      height: el?.clientHeight ?? 0,
-      width: el?.clientWidth ?? 0,
-    });
-  }, []);
 
   if (isLoading || data === null || blobUrl === null) {
     return null;
   }
 
-  const containerRatio = containerSize.height / containerSize.width;
-  const imageRatio = imageSize?.height / imageSize?.width;
-
   return (
-    <div ref={callbackRef} className="relative w-full h-full overflow-hidden">
-      <img
-        alt={alt}
-        className={classNames('absolute left-1/2 top-1/2 max-w-none transform -translate-x-1/2 -translate-y-1/2', {
-          'w-auto h-full': containerRatio > imageRatio,
-          'w-full h-auto': containerRatio <= imageRatio,
-        })}
-        src={blobUrl}
-      />
-    </div>
+    <img
+      alt={alt}
+      className="relative w-full h-full overflow-hidden object-cover"
+      src={blobUrl}
+    />
   );
 };
 
