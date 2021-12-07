6f874c078cb76056bac337a250c2ad1fc46c56e9
shellingford330 <pg.econ4869@gmail.com>
2021-12-07 23:03:01 +0900

    fix: image src from blob to url

--- client/src/components/foundation/CoveredImage/CoveredImage.jsx
+++ client/src/components/foundation/CoveredImage/CoveredImage.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import { useFetch } from '../../../hooks/use_fetch';
-import { fetchBinary } from '../../../utils/fetchers';
-
 /**
  * @typedef {object} Props
  * @property {string} alt
@@ -14,21 +11,11 @@ import { fetchBinary } from '../../../utils/fetchers';
  * @type {React.VFC<Props>}
  */
 const CoveredImage = ({ alt, src }) => {
-  const { data, isLoading } = useFetch(src, fetchBinary);
-
-  const blobUrl = React.useMemo(() => {
-    return data !== null ? URL.createObjectURL(new Blob([data])) : null;
-  }, [data]);
-
-  if (isLoading || data === null || blobUrl === null) {
-    return null;
-  }
-
   return (
     <img
       alt={alt}
       className="relative w-full h-full overflow-hidden object-cover"
-      src={blobUrl}
+      src={src}
     />
   );
 };
