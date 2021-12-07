2167fafa76fb27011771e5d9dc8a32e442726bb7
shellingford330 <pg.econ4869@gmail.com>
2021-12-07 23:56:47 +0900

    fix: remove no-trasform

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.raw({ limit: '10mb' }));
 
 app.use((_req, res, next) => {
   res.header({
-    'Cache-Control': 'max-age=0, no-transform',
+    'Cache-Control': 'max-age=0',
     Connection: 'close',
   });
   return next();
